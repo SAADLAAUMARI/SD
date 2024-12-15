9e9d3da17b1dbc0ff43b9f8998619aca85780250
SAAD <laaumaris@gmail.com>
2024-12-15 20:34:32 +0100

    CO commit

--- keycloakProject/ecom-app/inventory-service/src/main/java/com/laaumari/inventoryservice/web/ProductRestController.java
+++ keycloakProject/ecom-app/inventory-service/src/main/java/com/laaumari/inventoryservice/web/ProductRestController.java
@@ -2,16 +2,16 @@ package com.laaumari.inventoryservice.web;
 
 import com.laaumari.inventoryservice.entities.Product;
 import com.laaumari.inventoryservice.repository.ProductRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
+
 public class ProductRestController {
     private ProductRepository productRepository;
 
@@ -19,11 +19,17 @@ public class ProductRestController {
         this.productRepository = productRepository;
     }
     @GetMapping("/products")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public List<Product> productList(){
         return productRepository.findAll();
     }
     @GetMapping("/products/{id}")
+    @PreAuthorize("hasAuthority('USER')")
     public Product productById(@PathVariable String id){
         return productRepository.findById(id).get();
+    }
+    @GetMapping("/auth")
+    public Authentication authentication(Authentication authentication){
+        return authentication;
     }
 }
